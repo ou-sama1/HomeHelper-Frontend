7cb8f4f818db5d01892f5dac04293af52bb6c9ab
Oussama <xdooussama@gmail.com>
2024-08-23 16:39:44 +0100

    Make testimonials section responsive #21

--- src/pages/home/components/testimonials/index.jsx
+++ src/pages/home/components/testimonials/index.jsx
@@ -1,39 +1,49 @@
 import Reviews from "./Reviews";
 
 const data = [
-    {
-        id : 0,
-        reviewerImage : "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        reviewerName : "Salma ALAMI",
-        reviewText : "Je suis très satisfait du service de nettoyage. L'équipe a fait un travail impeccable, ma maison est propre et bien ordonnée. Je recommande vivement leurs services !",
-    },
-    {
-        id : 1,
-        reviewerImage : "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        reviewerName : "Mohammed FAHIM",
-        reviewText : "Excellente expérience avec ce plombier ! Professionnel, ponctuel et très efficace, il a résolu mon problème de fuite en un rien de temps. De plus, il a pris le temps de m'expliquer les travaux réalisés, ce que j'ai beaucoup apprécié. Je recommande vivement ses services pour leur qualité et leur sérieux.",
-    },
-    {
-        id : 2,
-        reviewerImage : "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        reviewerName : "Wissal FIKRI",
-        reviewText : "Service de peinture impeccable ! Travail soigné, équipe professionnelle et résultat parfait. Je recommande vivement.",
-    },
-    {
-        id : 3,
-        reviewerImage : "https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        reviewerName : "Oussama TALIBI",
-        reviewText : "Excellent service d'électricien ! Travail rapide, propre et professionnel. Je recommande fortement.",
-    },
+  {
+    id: 0,
+    reviewerImage:
+      "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    reviewerName: "Salma ALAMI",
+    reviewText:
+      "Je suis très satisfait du service de nettoyage. L'équipe a fait un travail impeccable, ma maison est propre et bien ordonnée. Je recommande vivement leurs services !",
+  },
+  {
+    id: 1,
+    reviewerImage:
+      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    reviewerName: "Mohammed FAHIM",
+    reviewText:
+      "Excellente expérience avec ce plombier ! Professionnel, ponctuel et très efficace, il a résolu mon problème de fuite en un rien de temps. De plus, il a pris le temps de m'expliquer les travaux réalisés, ce que j'ai beaucoup apprécié. Je recommande vivement ses services pour leur qualité et leur sérieux.",
+  },
+  {
+    id: 2,
+    reviewerImage:
+      "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    reviewerName: "Wissal FIKRI",
+    reviewText:
+      "Service de peinture impeccable ! Travail soigné, équipe professionnelle et résultat parfait. Je recommande vivement.",
+  },
+  {
+    id: 3,
+    reviewerImage:
+      "https://images.pexels.com/photos/5378700/pexels-photo-5378700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    reviewerName: "Oussama TALIBI",
+    reviewText:
+      "Excellent service d'électricien ! Travail rapide, propre et professionnel. Je recommande fortement.",
+  },
 ];
 
 const TestimonialsSection = () => {
-    return(
-        <section className="flex w-full flex-col px-10 py-16 gap-24 h-screen justify-center items-center">
-            <h1 className="font-bold text-6xl text-center">Découvrez ce que nos clients disent sur nous</h1>
-            <Reviews data={data} />
-        </section>
-    )
-}
+  return (
+    <section className="flex h-screen w-full flex-col items-center justify-center gap-10 px-0 py-16 lg:gap-24 lg:px-10">
+      <h1 className="px-1 text-center text-5xl font-bold lg:text-6xl">
+        Découvrez ce que nos clients disent sur nous
+      </h1>
+      <Reviews data={data} />
+    </section>
+  );
+};
 
 export default TestimonialsSection;
