3cd270abcffba166b49da83d4fcd375e78f2f706
Oussama <xdooussama@gmail.com>
2024-08-24 14:25:10 +0100

    Make forclients section responsive #24

--- src/pages/home/components/forClients/index.jsx
+++ src/pages/home/components/forClients/index.jsx
@@ -2,16 +2,23 @@ import { Link } from "react-router-dom";
 import { ArrowIcon } from "../../../../components/icons";
 
 const ForClientsSection = () => {
-    return(
-        <section className="flex w-full flex-col px-10 py-24 items-center bg-secondary text-white gap-5 text-center h-144 justify-center">
-            <h1 className="text-6xl font-bold">Prêt à mettre votre maison en parfait état ?</h1>
-            <p className="text-3xl font-medium">Découvrez nos différents services et réservez dès maintenant</p>
-            <Link to="/services" className="bg-primary text-3xl mt-10 px-10 py-5 rounded-lg font-bold flex items-center justify-between border transition hover:bg-secondary">
-                Nos services
-                <ArrowIcon  color={"#FFF"} dimensions={"50px"} direction={"right"} />
-            </Link>
-        </section>
-    )
-}
+  return (
+    <section className="flex w-full flex-col items-center justify-center gap-5 bg-secondary px-2 py-24 text-center text-white md:px-10 lg:h-144">
+      <h1 className="text-5xl font-bold lg:text-6xl">
+        Prêt à mettre votre maison en parfait état ?
+      </h1>
+      <p className="text-2xl font-medium lg:text-3xl">
+        Découvrez nos différents services et réservez dès maintenant
+      </p>
+      <Link
+        to="/services"
+        className="mt-10 flex items-center justify-between rounded-lg border bg-primary px-5 py-5 text-2xl font-bold transition hover:bg-secondary md:px-10 lg:text-3xl"
+      >
+        Nos services
+        <ArrowIcon color={"#FFF"} dimensions={"40px"} direction={"right"} />
+      </Link>
+    </section>
+  );
+};
 
 export default ForClientsSection;
